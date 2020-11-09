1fe6553b2075ef1220e1bd93ba172dc89c2ddb5b
Matthew Martin <zhintateo19924559@gmail.com>
2020-11-08 20:42:37 -0600

    Completed the Post Routes in the projectRouter

--- api/project/projectRouter.js
+++ api/project/projectRouter.js
@@ -3,6 +3,7 @@ const Project = require('../../data/helpers/projectModel');
 const Action = require('../../data/helpers/actionModel');
 const router = express.Router();
 
+// GET ROUTES
 router.get('/', (req, res) => {
     Project.get()
         .then(project => {
@@ -24,6 +25,45 @@ router.get('/:id/actions', validateProjectId, (req, res) => {
         .then(action => res.status(200).json(action))
         .catch(err=>res.status(500).json({mes:'server error', err}))
 })
+
+//POST ROUTES
+
+router.post('/',validateProject, (req, res) => {
+    Project.insert(req.body)
+        .then(project => res.status(201).json({mes: 'Project Created', project}))
+        .catch(err => res.status(500).json({mes:'Problem with the server', err}))
+} )
+
+router.post('/:id/actions', validateProjectId, validateActions, (req, res) =>{
+    const newAction = req.body;
+    newAction.project_id = req.params.id;
+    Action.insert(newAction)
+        .then(action => res.status(201).json(action))
+        .catch(err => res.status(500).json({mes:'server error', err}))
+})
+
+//UPDATE ROUTES
+
+
+
+function validateProject(req,res,next){
+    const project = req.body;
+
+    if (req.body && Object.keys(project).length >0){
+        next()
+    } else {
+        res.status(400).json({mes: 'No Body'})
+    }
+}
+function validateActions(req,res,next){
+    const project = req.body;
+
+    if (req.body && Object.keys(project).length >0){
+        next()
+    } else {
+        res.status(400).json({mes: 'No Body'})
+    }
+}
 
 function validateProjectId(req,res,next){
     const {id} = req.params;
